53b679b9c8f3d7d9ac65291e55bd24dc26abc6f3
Raphael Ramos <raphael.opensource@gmail.com>
2026-01-04 18:59:05 -0300

    feat: trocaregioes program

--- exercicios/trocaregioes.cpp
+++ exercicios/trocaregioes.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        std::cerr << "Uso: " << argv[0] << " <image_path>" << std::endl;
+        std::cerr << "Uso: " << argv[0] << " <imagem>" << std::endl;
         return -1;
     }
 
@@ -14,6 +14,26 @@ int main(int argc, char **argv)
         std::cerr << "Erro ao carregar a imagem!" << std::endl;
         return -1;
     }
+
+    // Extrair submatriz: sobrecargar do operador () da classe Mat
+    // Mat cv::Mat::operator()	(	Range	rowRange, Range	colRange ) const
+
+    // Copiar quadrantes da imagem
+    cv::Mat q1 = image(cv::Range(0, image.rows/2), cv::Range(0, image.cols/2)).clone();
+    cv::Mat q2 = image(cv::Range(0, image.rows/2), cv::Range(image.cols/2, image.cols)).clone();
+    cv::Mat q3 = image(cv::Range(image.rows/2, image.rows), cv::Range(0, image.cols/2)).clone();
+    cv::Mat q4 = image(cv::Range(image.rows/2, image.rows), cv::Range(image.cols/2, image.cols)).clone();
+
+    // Trocar quadrantes q1 com q4
+    q1.copyTo(image(cv::Range(image.rows/2, image.rows), cv::Range(image.cols/2, image.cols)));
+    q4.copyTo(image(cv::Range(0, image.rows/2), cv::Range(0, image.cols/2)));
+
+    // Trocar quadrantes q2 com q3
+    q2.copyTo(image(cv::Range(image.rows/2, image.rows), cv::Range(0, image.cols/2)));
+    q3.copyTo(image(cv::Range(0, image.rows/2), cv::Range(image.cols/2, image.cols)));
+    
+    cv::imshow("trocaregioes", image);
+    cv::waitKey(0);
 
     return 0;
 }
